c4ddbc935d48bf0e2993465b9f65cd5bcbf103a5
Geneviève Bastien <gbastien@versatic.net>
2025-12-10 10:09:42 -0500

    update webpack to not clean icons with production build
    fixes #13
    
    The `icons` path should also be kept after build

--- localisation/webpack.config.js
+++ localisation/webpack.config.js
@@ -127,7 +127,7 @@ module.exports = (env) => {
             new CleanWebpackPlugin({
                 dry: !isProduction,
                 verbose: true,
-                cleanAfterEveryBuildPatterns: ['**/*', '!images/**', '!documents/**', '!*.html']
+                cleanAfterEveryBuildPatterns: ['**/*', '!images/**', '!icons/**', '!documents/**', '!*.html']
             }),
             new HtmlWebpackPlugin({
                 filename: path.join(`index-survey-${config.projectShortname}.html`),
